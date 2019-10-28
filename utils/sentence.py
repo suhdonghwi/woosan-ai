cded986a4e796b5f911ff6fe47612333da3862ff
suhdonghwi <hwidongsuh@gmail.com>
2019-10-28 18:21:51 +0900

    Make similar_sentences function print pos too

--- utils/sentence.py
+++ utils/sentence.py
@@ -7,10 +7,10 @@ def similar_sentences(model, analyzer, sent):
         if pos in ['NNG', 'NP', 'NNP', 'VV', 'VA', 'VX', 'MAG', 'MAJ']:
             most_similar_words = [
                 word for (word, possibility) in model.wv.most_similar(word)]
-            for word in most_similar_words[:2]:
-                if pos[0] == analyzer.pos(word)[0][1][0]:
-                    similar_tags = [word if i == j else tag[0]
-                                    for (j, tag) in enumerate(tags)]
+            for similar_word in most_similar_words[:2]:
+                if pos[0] == analyzer.pos(similar_word)[0][1][0]:
+                    similar_tags = list(tags)
+                    similar_tags[i] = (similar_word, pos)
                     result.append(similar_tags)
 
     return result
